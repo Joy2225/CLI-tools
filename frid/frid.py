b43c217798823a3b6de3674665d9455bd65ba694
Joy2225 <archishman9892@gmail.com>
2025-05-03 16:26:18 +0530

    test time

--- frid/frid.py
+++ frid/frid.py
@@ -44,7 +44,7 @@ def adb_root(target_serial):
 
 
 def main():
-    print("Welcome to Frida AVD Launcher!")
+    print("Welcome to Frida AVD Launcher!!!")
     print("This script will start an AVD and run Frida server on it")
     avd = sys.argv[1].strip()
     if not avd:
